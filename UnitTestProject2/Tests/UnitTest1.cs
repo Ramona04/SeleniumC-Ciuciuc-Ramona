8f2bf7cf87acb63c33ac57b1dcb490c70528def0
Ramona04 <ciuciuc.ramona04@gmail.com>
2017-08-28 18:15:17 +0300

    Update UnitTest1.cs

--- UnitTestProject2/Tests/UnitTest1.cs
+++ UnitTestProject2/Tests/UnitTest1.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
-using NUnit.Framework;
-using UnitTestProject2.Pages;
+using System;
 using System.Threading;
+using UnitTestProject2.Pages;
 
 namespace UnitTestProject2
 {
@@ -18,16 +17,16 @@ namespace UnitTestProject2
             driver.Navigate().GoToUrl("http://www.cel.ro/");
 
             MainPage mainPage = new MainPage(driver);
-            NUnit.Framework.Assert.IsTrue(mainPage.CheckTitle().Contains("CEL"));
-            NUnit.Framework.Assert.IsTrue(mainPage.CheckLogoVisibility());
+            Assert.IsTrue(mainPage.CheckTitle().Contains("CEL"));
+            Assert.IsTrue(mainPage.CheckLogoVisibility());
             mainPage.SelectItem(7);
 
             SubMenuPage subMenu = new SubMenuPage(driver);
             subMenu.SelectSubItem();
 
             ProductsPage selectedPage = new ProductsPage(driver);
-            NUnit.Framework.Assert.IsTrue(selectedPage.CheckTitle().Contains("Antivirus"));
-            NUnit.Framework.Assert.AreEqual("CEL.ro / Software / Antivirus", selectedPage.CheckPath());
+            Assert.IsTrue(selectedPage.CheckTitle().Contains("Antivirus"));
+            Assert.AreEqual("CEL.ro / Software / Antivirus", selectedPage.CheckPath());
             selectedPage.SelectItem("Kaspersky Anti-Virus 2017 1PC 1An+3luni gratuite");
             selectedPage.Notification();
 
@@ -37,9 +36,9 @@ namespace UnitTestProject2
             antivirusPage.SeeCartContent();
 
             CartPage cartPage = new CartPage(driver);
-            NUnit.Framework.Assert.IsTrue(cartPage.CheckTitle().Contains("Continut"));
+            Assert.IsTrue(cartPage.CheckTitle().Contains("Continut"));
             cartPage.EditQuantity();
-            NUnit.Framework.Assert.AreEqual("300", cartPage.CheckFinalAmount());
+            Assert.AreEqual("300", cartPage.CheckFinalAmount());
 
             Console.WriteLine();
         }
